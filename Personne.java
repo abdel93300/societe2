35b61f5cd213a7dc7cb1ca9daaa28aef9be9b399
abdel93300 <abdelhafid93300@gmail.com>
2017-05-03 18:26:48 +0200

    Add files via upload
    Ajout des modifications du 3/5

--- Personne.java
+++ Personne.java
@@ -1,22 +1,30 @@
-
 public class Personne {
-	public Personne(String nom, String prenom, Integer age) {
+
+	private String prenom;
+	private String nom;
+	private Integer age;
+
+	public Personne(String nom, String prenom) {
+		super();
 		this.prenom = prenom;
 		this.nom = nom;
-		this.age=age;
-	}
-	
-	String prenom;
-	String nom;
-	Integer age;
-	
-	
-	public String toString() {
-		// TODO Auto-generated method stub
-		if (age != null) 
-		return prenom+" "+nom+ "age :" +age;
-		else return prenom+nom;		
 	}
 
-		
+	public Personne(String prenom, String nom, Integer age) {
+		super();
+		this.prenom = prenom;
+		this.nom = nom;
+		this.age = age;
 	}
+
+
+	public String toString() {
+		String description = prenom + " " + nom;
+		if (this.age != null)
+		 {
+			description = description + " : " + this.age + " an(s) ";
+		}
+		return description;
+	}
+
+}
--- Programme.java
+++ Programme.java
@@ -1,10 +1,11 @@
-
 	 public class Programme {
     	   public static void main()
     	      	  {
-    	  Personne formateur=new Personne("Eric","Siber",null);  	  
+    	  Personne formateur1=new Personne("Eric","Siber");  	  
+    	  Personne formateur2=new Personne("Nicolas","Hovart",19);  
     	  Personne apprennant=new Personne("Abdel","Nemmiche",51);
-    	  System.out.println(formateur);
+    	  System.out.println(formateur1);
+    	  System.out.println(formateur2);  
     	  System.out.println(apprennant);
     	  }
     	  
